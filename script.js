c39175ee2f5f9eda7cc9513c3307c60a71d20373
vinit-singh2007 <rajputvinitsingh2007@gmail.com>
2026-01-07 22:28:38 +0530

    added the script

---
+++ script.js
@@ -0,0 +1,25 @@
+let body=document.querySelector("body");
+body.onclick=()=>{
+    alert("This page is only consist of frontend(HTML,CSS),no backend is connected!!");
+}
+
+// let butn1=document.querySelector(".btn1");
+// let boxes=document.querySelectorAll(".box");
+// let mode="light";
+// butn1.addEventListener("click",()=>{
+//     if(mode==="light"){
+//         mode="dark";
+//         body.classList.add("dark");
+//         boxes.classList.add("dark");
+//         body.classList.remove("light");
+//         boxes.classList.remove("light");
+
+//     }else{
+//         mode="light";
+//         body.classList.add("light");
+//         boxes.classList.add("light");
+//         body.classList.remove("dark");
+//         boxes.classList.remove("dark");
+//     }
+//     console.log(mode);
+// });
